f02d4929c8dcef3662c7033e8be5ab85c92317e4
dindarosalin <dindarosalin24@gmail.com>
2023-10-25 21:47:21 +0700

    revisi

--- e2e/Liking_Restaurants_test.js
+++ e2e/Liking_Restaurants_test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
-  I.amOnPage('/#/like');
+  I.amOnPage('/#/favorite');
 });
 
 Scenario('showing empty liked Restaurants', ({ I }) => {
@@ -12,27 +12,27 @@ Scenario('showing empty liked Restaurants', ({ I }) => {
 
   // I.seeElement('.query'); --> menyebabkan error
 
-  I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
+  I.see('Tidak ada resto untuk ditampilkan', '.restaurant-item__not__found');
 });
 
 Scenario('liking one resto', async ({ I }) => {
-  I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
+  I.see('Tidak ada resto untuk ditampilkan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
 
   // pause();
 
-  I.seeElement('.resto__name a');
-  const firstResto = locate('.resto__name a').first();
+  I.seeElement('.restaurant__name a');
+  const firstResto = locate('.restaurant__name a').first();
   const firstRestoName = await I.grabTextFrom(firstResto);
   I.click(firstResto);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
-  I.amOnPage('/#/like');
-  I.seeElement('.resto-item');
-  const LikedRestoName = await I.grabTextFrom('.resto__name');
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+  const LikedRestoName = await I.grabTextFrom('.restaurant__name');
 
   assert.strictEqual(firstRestoName, LikedRestoName);
 });
