a8753da2f9bbc72a79370bea3c80d628167a9806
Dindaaa <dindarosalin24@gmail.com>
2023-12-25 21:07:25 +0700

    update hero

--- src/scripts/views/components/hero.js
+++ src/scripts/views/components/hero.js
@@ -5,15 +5,32 @@ class Hero extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `
-    
-    <div class="hero">
-        <div tabindex="0" class="hero_inner">
-            <h1 class="hero_title">SELAMAT MENCARI KEHIDUPAN</h1>
-            <p class="hero_tagline">Makan wajib, tempat bagus bonus</p>
-        </div>
-    </div>
-  `;
+    const heroDiv = document.createElement('div');
+    heroDiv.classList.add('hero');
+
+    const heroInnerDiv = document.createElement('div');
+    heroInnerDiv.classList.add('hero_inner');
+    heroInnerDiv.setAttribute('tabindex', '0');
+
+    const title = document.createElement('h1');
+    title.classList.add('hero_title');
+    title.textContent = 'SELAMAT MENCARI KEHIDUPAN';
+
+    const tagline = document.createElement('p');
+    tagline.classList.add('hero_tagline');
+    tagline.textContent = 'Makan wajib, tempat bagus bonus';
+
+    heroInnerDiv.appendChild(title);
+    heroInnerDiv.appendChild(tagline);
+    heroDiv.appendChild(heroInnerDiv);
+
+    // Set background image dynamically
+    heroDiv.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("../public/images/heros/hero-image_2.jpg")';
+    heroDiv.style.backgroundPosition = 'center';
+    heroDiv.style.backgroundSize = 'cover';
+    heroDiv.style.zIndex = '-1';
+
+    this.appendChild(heroDiv);
   }
 }
 
